07b8518578b422b46fe67a15f2c2a7f09404f765
ejjang2030 <ejjang2030@gmail.com>
2022-12-27 04:19:58 +0900

    feat : BookSearchFragment and ViewModel 책 검색 기능 추가

--- app/src/main/java/com/litholr/prolearner/ui/test/BookSearchFragment.kt
+++ app/src/main/java/com/litholr/prolearner/ui/test/BookSearchFragment.kt
@@ -2,13 +2,20 @@ package com.litholr.prolearner.ui.test
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import api.naver.BookResult
+import com.bumptech.glide.Glide
 import com.litholr.prolearner.R
+import com.litholr.prolearner.databinding.CardviewBookinfoBinding
 import com.litholr.prolearner.databinding.FragmentBookSearchBinding
 import com.litholr.prolearner.utils.PLToast
 
@@ -29,19 +36,55 @@ class BookSearchFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(BookSearchViewModel::class.java)
         binding = FragmentBookSearchBinding.inflate(LayoutInflater.from(requireContext()))
 
+        viewModel.results.observe(viewLifecycleOwner) {
+            Log.d(this.javaClass.simpleName, "viewModel.results changed to $it")
+        }
+        viewModel.books.observe(viewLifecycleOwner) {
+            binding.list.apply {
+                adapter = BookAdapter(it)
+                layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            }
+        }
+
         binding.search.setOnClickListener {
             PLToast.makeToast(
                 requireContext(),
                 "query : ${binding.query.text} / display : ${binding.number.text} / page : ${binding.page.text}",
                 Toast.LENGTH_SHORT)
             viewModel.searchBook(binding.query.text.toString(), binding.number.text.toString().toInt(), binding.page.text.toString().toInt())
-            binding.result.text = viewModel.results.value
         }
+
+
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+    }
+
+    class BookAdapter(val init: ArrayList<BookResult> = ArrayList()) : RecyclerView.Adapter<BookViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+            return BookViewHolder(CardviewBookinfoBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        }
+
+        override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+            holder.apply {
+                bindItem(init[position])
+            }
+        }
+
+        override fun getItemCount(): Int = init.size
+
+    }
+
+    class BookViewHolder(private val bookViewBinding: CardviewBookinfoBinding): RecyclerView.ViewHolder(bookViewBinding.root) {
+        fun bindItem(item: BookResult) {
+            Glide.with(bookViewBinding.root.context).load(item.image).into(bookViewBinding.bookImage)
+            bookViewBinding.title.text = item.title
+            bookViewBinding.author.text = item.author
+            bookViewBinding.publisher.text = item.publisher
+            bookViewBinding.description.text = item.description
+        }
     }
 }
--- app/src/main/java/com/litholr/prolearner/ui/test/BookSearchViewModel.kt
+++ app/src/main/java/com/litholr/prolearner/ui/test/BookSearchViewModel.kt
@@ -1,25 +1,48 @@
 package com.litholr.prolearner.ui.test
 
-import SecretId
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import api.naver.BookResult
 import api.naver.BookSearchResult
 import api.naver.NaverSearching
+import com.litholr.prolearner.utils.SecretId
 
 class BookSearchViewModel : ViewModel() {
     var query = MutableLiveData("")
+    var isInitial = MutableLiveData(true)
     var display = MutableLiveData(10)
     var page = MutableLiveData(1)
     var naver = NaverSearching(SecretId.NAVER_CLIENT_ID, SecretId.NAVER_CLIENT_ID_SECRET)
     var results = MutableLiveData("")
+    var books = MutableLiveData<ArrayList<BookResult>>(ArrayList())
+
+
     fun searchBook(query: String, display: Int, page: Int) {
+        if(query != this.query.value) {
+            this.query.postValue(query)
+            isInitial.postValue(true)
+        }
         naver.searchBook(query, display, page) { call, response, t ->
             if(response != null) {
                 if(response.isSuccessful) {
                     var result = response.body()
                     Log.d(this.javaClass.simpleName, "search : $result")
-                    results.postValue(response.toString())
+                    results.postValue(result.toString())
+                    if(result != null) {
+                        if(isInitial.value == true) {
+                            isInitial.postValue(false)
+                            books.postValue(ArrayList(result.items))
+                        } else {
+                            val old = books.value!!
+                            val array = ArrayList<BookResult>()
+                            array.addAll(old)
+                            array.addAll(ArrayList(result.items))
+                            books.postValue(array)
+                        }
+
+                    }
                 }
             }
         }
