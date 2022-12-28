e39c339eac8016d02a579d2354c3a15e4f75f2d9
ejjang2030 <ejjang2030@gmail.com>
2022-12-28 21:38:01 +0900

    fix : MainActivity fit with BaseActivity

--- app/src/main/java/com/litholr/prolearner/ui/main/MainActivity.kt
+++ app/src/main/java/com/litholr/prolearner/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.litholr.prolearner.ui.main
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Toast
+import com.litholr.prolearner.R
 import com.litholr.prolearner.databinding.ActivityMainBinding
 import com.litholr.prolearner.ui.base.BaseActivity
 import com.litholr.prolearner.utils.PLToast
@@ -10,12 +11,12 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
-    override val binding: ActivityMainBinding
-        get() = ActivityMainBinding.inflate(LayoutInflater.from(this))
+    override val layoutId: Int
+        get() = R.layout.activity_main
     override val viewModel: MainViewModel
         get() = MainViewModel()
 
     override fun onCreateBegin(savedInstanceState: Bundle?) {
-        PLToast.makeToast(this, "안녕하세요.", Toast.LENGTH_SHORT)
+
     }
 }
