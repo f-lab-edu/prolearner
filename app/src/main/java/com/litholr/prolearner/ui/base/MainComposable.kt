cc949786d1083944ad56116f1510a601b410abc0
ejjang2030 <ejjang2030@gmail.com>
2023-01-01 13:54:06 +0900

    add : Jetpack Compose component in HomeFragment for test

---
+++ app/src/main/java/com/litholr/prolearner/ui/base/MainComposable.kt
@@ -0,0 +1,9 @@
+package com.litholr.prolearner.ui.base
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainComposable() {
+    Text("Hello MainComposable!")
+}
--- app/src/main/java/com/litholr/prolearner/ui/home/HomeFragment.kt
+++ app/src/main/java/com/litholr/prolearner/ui/home/HomeFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.litholr.prolearner.R
 import com.litholr.prolearner.databinding.FragmentHomeBinding
 import com.litholr.prolearner.ui.base.BaseFragment
+import com.litholr.prolearner.ui.base.MainComposable
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val layoutId: Int
         get() = R.layout.fragment_home
 
     override fun onCreateBegin(savedInstanceState: Bundle?) {
+        binding.composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MainComposable()
+            }
+        }
     }
 }
