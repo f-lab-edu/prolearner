62f849cd3bb12b8d58cb302382ca9882b153e15c
ejjang2030 <ejjang2030@gmail.com>
2023-01-11 23:43:45 +0900

    modified : SearchFragment를 MainActivity에 적용(viewmodel 사용)

--- app/src/main/java/com/litholr/prolearner/ui/main/MainActivity.kt
+++ app/src/main/java/com/litholr/prolearner/ui/main/MainActivity.kt
@@ -1,51 +1,140 @@
 package com.litholr.prolearner.ui.main
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.content.res.AppCompatResources
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.marginEnd
+import androidx.core.view.marginStart
+import androidx.core.view.updateLayoutParams
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.litholr.prolearner.R
 import com.litholr.prolearner.databinding.ActivityMainBinding
+import com.litholr.prolearner.databinding.AppbarSearchBinding
 import com.litholr.prolearner.ui.base.BaseActivity
+import com.litholr.prolearner.ui.test.BookSearchViewModel
 import com.litholr.prolearner.utils.PLToast
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.coroutines.coroutineContext
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+
     override val layoutId: Int
         get() = R.layout.activity_main
-    override val viewModel: MainViewModel
-        get() = MainViewModel()
+    override val viewModel: MainViewModel by viewModels()
 
-//    lateinit var navHostFragment: NavHostFragment
-//    lateinit var navController: NavController
-
-    override fun onCreateBegin(savedInstanceState: Bundle?) {
-//        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-//        navController = navHostFragment.navController
-//        binding.bottomNavigationBar.setupWithNavController(navController)
-//        setListeners()
-        val logo = ImageView(this)
-        logo.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.appbar_logo))
-        binding.appbar.addView(logo)
+    companion object {
+        val mainViewModel: MainViewModel
+            get() = MainViewModel()
     }
 
-    private fun setListeners() {
+    lateinit var navHostFragment: NavHostFragment
+    lateinit var navController: NavController
+
+    override fun onCreateBegin(savedInstanceState: Bundle?) {
+        setNavigation()
+        toHome()
+        setBottomNavigationBar()
+        setObservables()
+    }
+
+    private fun setNavigation() {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationBar.setupWithNavController(navController)
+    }
+
+    private fun setBottomNavigationBar() {
+        binding.bottomNavigationBar.setOnNavigationItemSelectedListener {
+            binding.appbar.removeAllViews()
+            when(it.itemId) {
+                R.id.home -> {
+                    toHome()
+                    true
+                }
+                R.id.search -> {
+                    toSearch()
+                    true
+                }
+                R.id.profile -> {
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setObservables() {
+//        viewModel.query.observe(this) {
+//            Log.d(this.javaClass.simpleName, "query : ${it}")
+//            mainViewModel.searchBook(it)
+//        }
+//        viewModel.books.observe(this) {
+//            Log.d(this.javaClass.simpleName, "setObservables() books -> ${it}")
+//        }
+        viewModel.page.observe(this) {
+            viewModel.searchBook(viewModel.query.value!!, 10, it)
+        }
+    }
+
+    private fun toHome() {
+        binding.appbar.addView(
+            ImageView(this@MainActivity).apply {
+                setImageDrawable(AppCompatResources.getDrawable(this@MainActivity, R.drawable.appbar_logo))
+                layoutParams = LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                ).apply {
+                    setMargins(12 * 4, 0, 0,0)
+                    gravity = Gravity.CENTER_VERTICAL
+                }
+            }
+        )
+        navController.navigate(R.id.toHomeFragment)
+    }
+
+    private fun toSearch() {
+        val appbarSearchBinding = AppbarSearchBinding.inflate(LayoutInflater.from(this@MainActivity)).apply {
+            root.apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+//            search.addTextChangedListener(object: TextWatcher {
+//                override fun afterTextChanged(s: Editable?) {
+//                }
+//
+//                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//                    Log.d(this.javaClass.simpleName, "beforeTextChanged : ${ s.toString() }")
+//                }
+//
+//                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                    Log.d(this.javaClass.simpleName, "onTextChanged : ${ s.toString() }")
+//                    viewModel.query.postValue(s.toString())
+//                }
+//            })
+            button.setOnClickListener {
+//                mainViewModel.query.postValue(search.text.toString())
+                viewModel.searchBook(search.text.toString())
+            }
+        }
+        binding.appbar.addView(appbarSearchBinding.root)
+        navController.navigate(R.id.toSearchFragment)
     }
 }
--- app/src/main/java/com/litholr/prolearner/ui/main/MainViewModel.kt
+++ app/src/main/java/com/litholr/prolearner/ui/main/MainViewModel.kt
@@ -1,9 +1,48 @@
 package com.litholr.prolearner.ui.main
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import api.naver.BookResult
+import api.naver.NaverSearching
 import com.litholr.prolearner.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
 @HiltViewModel
 class MainViewModel: BaseViewModel() {
+    var query = MutableLiveData("")
+    var isInitial = MutableLiveData(true)
+    var display = MutableLiveData(10)
+    var page = MutableLiveData(1)
+    var naver = NaverSearching(SecretId.NAVER_CLIENT_ID, SecretId.NAVER_CLIENT_ID_SECRET)
+    var results = MutableLiveData("")
+    var books = MutableLiveData<ArrayList<BookResult>>(ArrayList())
 
+    fun searchBook(query: String, display: Int = 10, page: Int = 1) {
+        if(query != this.query.value) {
+            this.query.postValue(query)
+            isInitial.postValue(true)
+        }
+        naver.searchBook(query, display, page, "date") { call, response, t ->
+            if(response != null) {
+                if(response.isSuccessful) {
+                    var result = response.body()
+                    Log.d(this.javaClass.simpleName, "search : $result")
+                    results.postValue(result.toString())
+                    if(result != null) {
+                        if(isInitial.value == true) {
+                            isInitial.postValue(false)
+                            books.postValue(ArrayList(result.items))
+                        } else {
+                            val old = books.value!!
+                            val array = ArrayList<BookResult>()
+                            array.addAll(old)
+                            array.addAll(ArrayList(result.items))
+                            books.postValue(array)
+                        }
+
+                    }
+                }
+            }
+        }
+    }
 }
