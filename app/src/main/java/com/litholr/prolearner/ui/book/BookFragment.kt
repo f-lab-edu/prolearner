c870b67c11edf1352122d0c73f19bc015d34ba03
ejjang2030 <ejjang2030@gmail.com>
2023-01-19 19:54:20 +0900

    add : 책 검색해서 선택하여 책 목록 보여주는 화면 구현

---
+++ app/src/main/java/com/litholr/prolearner/ui/book/BookFragment.kt
@@ -0,0 +1,76 @@
+package com.litholr.prolearner.ui.book
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Toast
+import androidx.compose.ui.unit.dp
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import api.naver.BookResult
+import com.bumptech.glide.Glide
+import com.ejjang2030.bookcontentparser.BookContentParser
+import com.litholr.prolearner.R
+import com.litholr.prolearner.databinding.BookContentItemBinding
+import com.litholr.prolearner.databinding.CardviewBookinfoBinding
+import com.litholr.prolearner.databinding.FragmentBookBinding
+import com.litholr.prolearner.ui.base.BaseFragment
+import com.litholr.prolearner.ui.main.MainViewModel
+import com.litholr.prolearner.utils.PLToast
+
+class BookFragment: BaseFragment<FragmentBookBinding>() {
+    override val layoutId: Int
+        get() = R.layout.fragment_book
+
+    val mainViewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateBegin(savedInstanceState: Bundle?) {
+        mainViewModel.selectedBook.observe(this) {
+            Glide.with(binding.root.context).load(it.image).into(binding.bookImage)
+            binding.bookTitle.text = it.title
+            binding.bookAuthor.text = it.author
+            binding.bookPublisher.text = it.publisher
+            binding.bookDescription.text = it.description
+            mainViewModel.naver.getBookCatalog(it) { call, res, catalog, t ->
+                if(res != null) {
+                    if(res.isSuccessful) {
+                        if(catalog != null) {
+                            val contentTable = catalog.descriptions.contentTable
+                            val list: MutableList<String> = BookContentParser.getBookContentTableList(contentTable)
+                            binding.bookContents.apply {
+                                adapter = BookContentAdapter(list)
+                                layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    inner class BookContentAdapter(val array: List<String> = ArrayList()) : RecyclerView.Adapter<BookContentViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookContentViewHolder {
+            return BookContentViewHolder(BookContentItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        }
+
+        override fun onBindViewHolder(holder: BookContentViewHolder, position: Int) {
+            holder.apply {
+                bindItem(array[position])
+            }
+        }
+
+        override fun getItemCount(): Int = array.size
+    }
+
+    inner class BookContentViewHolder(private val bookContentItemBinding: BookContentItemBinding): RecyclerView.ViewHolder(bookContentItemBinding.root) {
+        fun bindItem(title: String) {
+            bookContentItemBinding.contentTitle.text = title
+        }
+    }
+}
