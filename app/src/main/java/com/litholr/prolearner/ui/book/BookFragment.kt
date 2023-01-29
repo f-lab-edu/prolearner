e1567cf3b4565e89defa20edf55cd42aee8c297f
ejjang2030 <ejjang2030@gmail.com>
2023-01-29 16:11:43 +0900

    modified : TabLayout을 위한 ViewPager2 적용

--- app/src/main/java/com/litholr/prolearner/ui/book/BookFragment.kt
+++ app/src/main/java/com/litholr/prolearner/ui/book/BookFragment.kt
@@ -10,14 +10,18 @@ import android.widget.Toast
 import androidx.compose.ui.unit.dp
 import androidx.core.view.drawToBitmap
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.MutableLiveData
 import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import api.naver.BookResult
 import com.bumptech.glide.Glide
 import com.ejjang2030.bookcontentparser.BookContentParser
+import com.google.android.material.tabs.TabLayoutMediator
 import com.litholr.prolearner.R
 import com.litholr.prolearner.databinding.BookContentItemBinding
 import com.litholr.prolearner.databinding.CardviewBookinfoBinding
@@ -33,9 +37,11 @@ class BookFragment: BaseFragment<FragmentBookBinding>() {
 
     val mainViewModel: MainViewModel by activityViewModels()
 
-    override fun onCreateBegin(savedInstanceState: Bundle?) {
-        mainViewModel.selectedBook.observe(this) {
+    lateinit var viewPager2Adapter: ViewPager2Adapter
 
+    override fun onCreateBegin(savedInstanceState: Bundle?) {
+        initViewPage()
+        mainViewModel.selectedBook.observe(this) {
             mainViewModel.naver.getBookCatalog(it) { bresult, catalog, call, res, t ->
                 if(res != null) {
                     if(res.isSuccessful) {
@@ -53,16 +59,56 @@ class BookFragment: BaseFragment<FragmentBookBinding>() {
                             binding.bookDescription.text = catalog.description
                             val contentTable = catalog.contentsHtml
                             val list: MutableList<String> = BookContentParser.getBookContentTableList(contentTable)
-                            val contentList = catalog.getBookContentTableList()
-                            binding.bookContents.apply {
-                                adapter = BookContentAdapter(contentList)
-                                layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
-                            }
+                            val contentLists = catalog.getBookContentTableList()
+                            viewPager2Adapter.addFragment(ContentsFragment(contentLists))
+                            viewPager2Adapter.addFragment(GoalsFragment())
                         }
                     }
                 }
-
             }
+        }
+    }
+
+    private fun initViewPage() {
+        viewPager2Adapter = ViewPager2Adapter(this)
+        binding.viewpagerMain.apply {
+            adapter = viewPager2Adapter
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                }
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+                    super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                }
+            })
+        }
+
+        TabLayoutMediator(binding.tabNavigationView, binding.viewpagerMain) { tab, position ->
+            when(position) {
+                0 -> tab.text = "학습내용"
+                1 -> tab.text = "목표설정"
+            }
+        }.attach()
+    }
+
+    inner class ViewPager2Adapter(fragment: Fragment): FragmentStateAdapter(fragment) {
+        var fragments: ArrayList<Fragment> = ArrayList()
+
+        override fun getItemCount(): Int {
+            return fragments.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        fun addFragment(fragment: Fragment) {
+            fragments.add(fragment)
+            notifyItemInserted(fragments.size - 1)
         }
     }
 
