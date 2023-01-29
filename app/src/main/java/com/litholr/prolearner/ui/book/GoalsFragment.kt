82d7c20b283640d82d4be7f1366448062111a2c4
ejjang2030 <ejjang2030@gmail.com>
2023-01-29 21:49:30 +0900

    modified : GoalsFragment 시작일 종료일 선택할 수 있도록 구현

--- app/src/main/java/com/litholr/prolearner/ui/book/GoalsFragment.kt
+++ app/src/main/java/com/litholr/prolearner/ui/book/GoalsFragment.kt
@@ -1,8 +1,11 @@
 package com.litholr.prolearner.ui.book
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,12 +14,63 @@ import com.litholr.prolearner.databinding.BookContentItemBinding
 import com.litholr.prolearner.databinding.FragmentContentsBinding
 import com.litholr.prolearner.databinding.FragmentGoalsBinding
 import com.litholr.prolearner.ui.base.BaseFragment
+import com.litholr.prolearner.utils.CustomDatePicker
+import java.util.*
 
 class GoalsFragment(): BaseFragment<FragmentGoalsBinding>() {
     override val layoutId: Int
         get() = R.layout.fragment_goals
 
     override fun onCreateBegin(savedInstanceState: Bundle?) {
+        binding.startDate.setOnClickListener {
+            val datePicker = CustomDatePicker(requireActivity(), object: CustomDatePicker.ICustomDateListener {
+                override fun onCancel() {
+                }
 
+                override fun onSet(
+                    dialog: Dialog,
+                    calendarSelected: Calendar,
+                    dateSelected: Date,
+                    year: Int,
+                    monthFullName: String,
+                    monthShortName: String,
+                    monthNumber: Int,
+                    day: Int,
+                    weekDayFullName: String,
+                    weekDayShortName: String
+                ) {
+                    binding.startDatePick.text = "$year.${monthNumber + 1}.$day"
+                }
+
+            }).apply {
+                setDate(Calendar.getInstance())
+                showDialog()
+            }
+        }
+        binding.endDate.setOnClickListener {
+            val datePicker = CustomDatePicker(requireActivity(), object: CustomDatePicker.ICustomDateListener {
+                override fun onCancel() {
+                }
+
+                override fun onSet(
+                    dialog: Dialog,
+                    calendarSelected: Calendar,
+                    dateSelected: Date,
+                    year: Int,
+                    monthFullName: String,
+                    monthShortName: String,
+                    monthNumber: Int,
+                    day: Int,
+                    weekDayFullName: String,
+                    weekDayShortName: String
+                ) {
+                    binding.endDatePick.text = "$year.${monthNumber + 1}.$day"
+                }
+
+            }).apply {
+                setDate(Calendar.getInstance())
+                showDialog()
+            }
+        }
     }
 }
