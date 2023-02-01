210fb204dc70f8e4bc9f86011bf4ed802d2df8c4
ejjang2030 <ejjang2030@gmail.com>
2023-02-01 21:55:53 +0900

    modified : PLToast 디자인 변경

--- app/src/main/java/com/litholr/prolearner/utils/PLToast.kt
+++ app/src/main/java/com/litholr/prolearner/utils/PLToast.kt
@@ -15,14 +15,12 @@ object PLToast {
             toast.duration = duration
             val binding: LayoutToastBinding = LayoutToastBinding.inflate(LayoutInflater.from(context))
             binding.message.text = message
-            binding.message.setTextColor(context.getResources().getColor(R.color.purple_200))
             toast.view = binding.root
             return setToast(toast).show()
         }
     }
 
     private fun setToast(toast: Toast): Toast {
-        toast
         return toast
     }
 }
