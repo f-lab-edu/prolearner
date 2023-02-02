f99a85b4b90f4cc4a047a60d2e33114e232b19ff
ejjang2030 <ejjang2030@gmail.com>
2023-02-02 20:26:12 +0900

    modified : BookFragment 최대한 Databinding쓰도록 구현

--- app/src/main/java/com/litholr/prolearner/ui/book/BookFragment.kt
+++ app/src/main/java/com/litholr/prolearner/ui/book/BookFragment.kt
@@ -1,25 +1,17 @@
 package com.litholr.prolearner.ui.book
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.MutableLiveData
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
-import com.ejjang2030.bookcontentparser.BookContentParser
 import com.google.android.material.tabs.TabLayoutMediator
 import com.litholr.prolearner.R
-import com.litholr.prolearner.databinding.BookContentItemBinding
 import com.litholr.prolearner.databinding.FragmentBookBinding
 import com.litholr.prolearner.ui.base.BaseFragment
 import com.litholr.prolearner.ui.main.MainViewModel
-import com.litholr.prolearner.utils.PLToast
 
 class BookFragment: BaseFragment<FragmentBookBinding>() {
     override val layoutId: Int
@@ -31,18 +23,22 @@ class BookFragment: BaseFragment<FragmentBookBinding>() {
 
     override fun onCreateBegin(savedInstanceState: Bundle?) {
         initViewPage()
-        mainViewModel.selectedBook.observe(this) {
-            mainViewModel.naver.getBookCatalog(it) { bresult, catalog, call, res, t ->
-                if((res != null) && res.isSuccessful) {
-                    if(catalog != null) {
-                        Glide.with(binding.root.context).load(it.image).into(binding.bookImage)
-                        binding.bookTitle.text = catalog.title
-                        if(catalog.subtitle != null) {
-                            binding.bookSubtitle.visibility = View.VISIBLE
-                            binding.bookSubtitle.text = catalog.subtitle
-                        } else {
-                            binding.bookSubtitle.visibility = View.GONE
+        mainViewModel.selectedBook.observe(this) { bookResult ->
+            mainViewModel.naver.getBookCatalog(bookResult) { bresult, catalog, call, res, t ->
+                res?.let {
+                    if(it.isSuccessful) {
+                        if(catalog == null) {
+                            mainViewModel.plToastListener?.printToast("책의 정보를 불러오지 못했습니다.")
+                            mainViewModel.bottomNav.value = MainViewModel.BottomNav.SEARCH
+                            return@getBookCatalog
                         }
+                        val contentLists = catalog.getBookContentTableList()
+                        if(contentLists == null) {
+                            mainViewModel.plToastListener?.printToast("목차정보가 없습니다.")
+                            return@getBookCatalog
+                        }
+                        Glide.with(binding.root.context).load(bresult.image).into(binding.bookImage)
+                        binding.catalog = catalog
                         binding.infoAuthor.apply {
                             title.text = "저자"
                             text.text = catalog.authorList.joinToString(", ")
@@ -55,21 +51,9 @@ class BookFragment: BaseFragment<FragmentBookBinding>() {
                             title.text = "정보제공"
                             text.text = catalog.contentsSourceMallName ?: "미확인"
                         }
-
                         binding.expandTextView.setOnExpandStateChangeListener { textView, isExpanded -> }
-                        binding.expandTextView.text = catalog.description
-                        binding.expandableText.text = catalog.description
-                        val contentTable = catalog.contentsHtml
-                        val contentLists = catalog.getBookContentTableList()
-                        if(contentLists != null) {
-                            viewPager2Adapter.addFragment(ContentsFragment(contentLists))
-                            viewPager2Adapter.addFragment(GoalsFragment())
-                        } else {
-                            mainViewModel.plToastListener?.printToast("목차정보가 없습니다.")
-                        }
-                    } else {
-                        mainViewModel.plToastListener?.printToast("책의 정보를 불러오지 못했습니다.")
-                        mainViewModel.bottomNav.value = MainViewModel.BottomNav.SEARCH
+                        viewPager2Adapter.addFragment(ContentsFragment(contentLists))
+                        viewPager2Adapter.addFragment(GoalsFragment())
                     }
                 }
             }
