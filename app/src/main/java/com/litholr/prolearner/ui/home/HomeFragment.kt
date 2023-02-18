151619fcb178b0bb70cb161805ff348d04e2e025
ejjang2030 <ejjang2030@gmail.com>
2023-02-18 23:23:54 +0900

    modified : HomeFragment LocalDB에서 가져와서 책 목록 보여주기

--- app/src/main/java/com/litholr/prolearner/ui/home/HomeFragment.kt
+++ app/src/main/java/com/litholr/prolearner/ui/home/HomeFragment.kt
@@ -1,27 +1,68 @@
 package com.litholr.prolearner.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.litholr.prolearner.R
+import com.litholr.prolearner.data.local.entity.SavedBookInfo
+import com.litholr.prolearner.databinding.CardviewSavedbookinfoBinding
 import com.litholr.prolearner.databinding.FragmentHomeBinding
 import com.litholr.prolearner.ui.base.BaseFragment
+import com.litholr.prolearner.ui.main.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val layoutId: Int
         get() = R.layout.fragment_home
 
-    override fun onCreateBegin(savedInstanceState: Bundle?) {
+    val mainViewModel: MainViewModel by activityViewModels()
 
+    override fun onCreateBegin(savedInstanceState: Bundle?) {
+        loadSavedBookList()
+    }
+
+    private fun loadSavedBookList() {
+        lifecycleScope.launch {
+            mainViewModel.getSavedBookInfos().observe(this@HomeFragment) {
+                binding.savedBookList.apply {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        adapter = SavedBookAdapter(it)
+                    }
+                    layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+                }
+            }
+        }
+    }
+
+    inner class SavedBookAdapter(val array: List<SavedBookInfo>) : RecyclerView.Adapter<SavedBookViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedBookViewHolder {
+            return SavedBookViewHolder(CardviewSavedbookinfoBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        }
+        override fun onBindViewHolder(holder: SavedBookViewHolder, position: Int) {
+            holder.bindItem(array[position])
+        }
+        override fun getItemCount(): Int = array.size
+    }
+    inner class SavedBookViewHolder(private val savedBookViewBinding: CardviewSavedbookinfoBinding): RecyclerView.ViewHolder(savedBookViewBinding.root) {
+        fun bindItem(item: SavedBookInfo) {
+            item.catalog?.let {
+                Glide.with(savedBookViewBinding.root.context).load(it.thumbnailUrl).into(savedBookViewBinding.bookImage)
+                savedBookViewBinding.bookCategory.text = it.categoryName
+                savedBookViewBinding.title.text = it.title
+                savedBookViewBinding.author.text = it.authorList.joinToString(", ")
+                savedBookViewBinding.publisher.text = it.publisher
+                savedBookViewBinding.root.setOnClickListener {
+                    mainViewModel.updateBottomNavToBook(item.bookResult, false)
+                }
+            }
+        }
     }
 }
 
